90bfc5fe0c49d509afc0c604d369e7b68e803fd5
xuhongzhe <xuhongzhe@kuaishou.com>
2021-10-08 20:28:18 +0800

    init

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const CracoAntDesignPlugin = require('craco-antd');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        customizeTheme: {
+          '@primary-color': '#1DA57A',
+        },
+      },
+    },
+  ],
+};
