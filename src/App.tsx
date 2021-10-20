f5e08a980617999725c5efedb1cda01a52d81c38
xuhongzhe <xuhongzhe@kuaishou.com>
2021-10-20 18:45:15 +0800

    feat: 搭建基础架构

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import { Button } from 'antd'
+import React, { useState, useEffect } from 'react';
+import { Button } from 'antd';
 import './App.less';
 
+const api = require('./mock/api.json')
+
 function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList(api.data)
+  }, []);
+
   return (
     <div className="App">
-      <Button type='primary'>antd</Button>
+      <div className="header">
+        <span>Miss xx</span>
+      </div>
+
+      <div className="content">
+        {list.map((item: any) => (
+          <div className="list-item">
+            <span className="time">{item.time}</span>
+            <span className="text">{item.text}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
